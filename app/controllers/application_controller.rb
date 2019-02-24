a575075f16fbdc09adeeb49b91fb12cadd73efd0
vutivi <201141790@student.uj.ac.za>
2019-02-25 01:17:39 +0200

    chore: setup authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_methods :logged_in?, :current_user
+
+  def current_user
+    @current_user ||= User.find_by(username: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in
+      flash[:error] = "Not Authorized"
+      redirect_to sign_in_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user
-  get 'chatroom', to: 'chatroom#index'
-  get 'sign_in', to: 'session#new', as: 'sign_in'
+  get  'chatroom', to: 'chatroom#index'
+  get  'sign_in', to: 'session#new', as: 'sign_in'
+  post 'sign_in', to: 'session#create'
   root to: 'chatroom#index'
 end
