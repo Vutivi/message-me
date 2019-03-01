cad3733ae29e69b0f45991445ee2e124b27bfb52
vutivi <201141790@student.uj.ac.za>
2019-03-01 13:36:06 +0200

    chore: add validation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,11 @@
 class UserController < ApplicationController
   def new
-    @user = User.new
+    if logged_in?
+      flash[:positive] = "Already signed in!"
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def show
@@ -9,13 +14,18 @@ class UserController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      flash[:positive] = "Welcome to Message me. Start sending messages!"
-      redirect_to root_path
-    else
-      flash[:negative] = "Error registering user!"
+    if params[:user][:password] != params[:user][:confirm_password]
+      flash[:negative] = "Passwords are not the same."
       render :new
+    else
+      if user.save
+        session[:user_id] = user.id
+        flash[:positive] = "Welcome to Message me. Start sending messages!"
+        redirect_to root_path
+      else
+        flash[:negative] = "Error registering user!"
+        render :new
+      end
     end
   end
 
